765bd35c74c0e8e495a3a995b7ea1f0d1c8c9927
niahikawakairi <kairi.0109@au.com>
2022-05-18 10:10:01 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,2 @@
 class BooksController < ApplicationController
-  def new
-  end
-
-  def index
-  end
-
-  def show
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,2 @@
 class UsersController < ApplicationController
-  def show
-  end
-
-  def edit
-  end
 end
